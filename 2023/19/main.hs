e0cf86c3c6abca67615d8036c38cbc1aa54c475e
Pol Eyschen <poleyschen@gmail.com>
2023-12-19 22:17:14 +0100

    2023 Day 19

--- 2023/19/main.hs
+++ 2023/19/main.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE BangPatterns #-}
+
 module Main (main) where
 
 import Data.Either (fromRight)
 import Data.List.Split (splitOn)
 import qualified Data.Map as M
+import qualified Data.Time as T
 import Text.Parsec as P
 import Text.Parsec.String (Parser)
 
@@ -98,11 +101,83 @@ parseParts = map (fromRight (error "Workflow parser error") . P.parse parsePart 
 part1 :: M.Map Destination Workflow -> [Part] -> Int
 part1 w p = sum $ map (runPart w) p
 
+type Range = (Int, Int)
+
+data PartRange = PartRange Range Range Range Range deriving (Eq)
+
+getConditionRange :: Condition -> PartRange -> (Maybe PartRange, Maybe PartRange)
+getConditionRange (ConditionLT X x) (PartRange (x1, x2) m a s)
+  | x <= x1 = (Nothing, Just $ PartRange (x1, x2) m a s)
+  | x > x2 = (Just $ PartRange (x1, x2) m a s, Nothing)
+  | otherwise = (Just $ PartRange (x1, x - 1) m a s, Just $ PartRange (x, x2) m a s)
+getConditionRange (ConditionLT M m) (PartRange x (m1, m2) a s)
+  | m <= m1 = (Nothing, Just $ PartRange x (m1, m2) a s)
+  | m > m2 = (Just $ PartRange x (m1, m2) a s, Nothing)
+  | otherwise = (Just $ PartRange x (m1, m - 1) a s, Just $ PartRange x (m, m2) a s)
+getConditionRange (ConditionLT A a) (PartRange x m (a1, a2) s)
+  | a <= a1 = (Nothing, Just $ PartRange x m (a1, a2) s)
+  | a > a2 = (Just $ PartRange x m (a1, a2) s, Nothing)
+  | otherwise = (Just $ PartRange x m (a1, a - 1) s, Just $ PartRange x m (a, a2) s)
+getConditionRange (ConditionLT S s) (PartRange x m a (s1, s2))
+  | s <= s1 = (Nothing, Just $ PartRange x m a (s1, s2))
+  | s > s2 = (Just $ PartRange x m a (s1, s2), Nothing)
+  | otherwise = (Just $ PartRange x m a (s1, s - 1), Just $ PartRange x m a (s, s2))
+getConditionRange (ConditionGT X x) (PartRange (x1, x2) m a s)
+  | x < x1 = (Just $ PartRange (x1, x2) m a s, Nothing)
+  | x >= x2 = (Nothing, Just $ PartRange (x1, x2) m a s)
+  | otherwise = (Just $ PartRange (x + 1, x2) m a s, Just $ PartRange (x1, x) m a s)
+getConditionRange (ConditionGT M m) (PartRange x (m1, m2) a s)
+  | m < m1 = (Just $ PartRange x (m1, m2) a s, Nothing)
+  | m >= m2 = (Nothing, Just $ PartRange x (m1, m2) a s)
+  | otherwise = (Just $ PartRange x (m + 1, m2) a s, Just $ PartRange x (m1, m) a s)
+getConditionRange (ConditionGT A a) (PartRange x m (a1, a2) s)
+  | a < a1 = (Just $ PartRange x m (a1, a2) s, Nothing)
+  | a >= a2 = (Nothing, Just $ PartRange x m (a1, a2) s)
+  | otherwise = (Just $ PartRange x m (a + 1, a2) s, Just $ PartRange x m (a1, a) s)
+getConditionRange (ConditionGT S s) (PartRange x m a (s1, s2))
+  | s < s1 = (Just $ PartRange x m a (s1, s2), Nothing)
+  | s >= s2 = (Nothing, Just $ PartRange x m a (s1, s2))
+  | otherwise = (Just $ PartRange x m a (s + 1, s2), Just $ PartRange x m a (s1, s))
+
+getRuleRanges :: [Rule] -> Maybe PartRange -> Destination -> [(PartRange, Destination)]
+getRuleRanges _ Nothing _ = []
+getRuleRanges [] (Just r) def = [(r, def)]
+getRuleRanges (Rule c n : rs) (Just r) def = case getConditionRange c r of
+  (Nothing, Just r') -> getRuleRanges rs (Just r') def
+  (Just r', Nothing) -> [(r', n)]
+  (Just r1, Just r2) -> (r1, n) : getRuleRanges rs (Just r2) def
+  _ -> error "lolno"
+
+getWorkflowRange :: M.Map Destination Workflow -> PartRange -> Destination -> [(PartRange, Destination)]
+getWorkflowRange _ r Accepted = [(r, Accepted)]
+getWorkflowRange _ r Rejected = [(r, Rejected)]
+getWorkflowRange ws pr n = case M.lookup n ws of
+  Just (Workflow rs def) -> getRuleRanges rs (Just pr) def
+  Nothing -> error "No Workflow found"
+
+runRanges :: M.Map Destination Workflow -> PartRange -> [(PartRange, Destination)]
+runRanges ws pr = filter (\(_, d) -> d == Accepted) $ until (all (\(_, d) -> d == Accepted || d == Rejected)) (concatMap (uncurry (getWorkflowRange ws))) [(pr, Name "in")]
+
+part2 :: M.Map Destination Workflow -> Int
+part2 ws =
+  sum $
+    map ((\(PartRange (x1, x2) (m1, m2) (a1, a2) (s1, s2)) -> (x2 - x1 + 1) * (m2 - m1 + 1) * (a2 - a1 + 1) * (s2 - s1 + 1)) . fst) $
+      runRanges ws $
+        PartRange (1, 4000) (1, 4000) (1, 4000) (1, 4000)
+
 main :: IO ()
 main = do
   inp <- splitOn [""] <$> loadInput "19/input.txt"
-  let workflows = parseWorkflows $ head inp
-  let parts = parseParts $ last inp
 
-  print $ part1 workflows parts
-  putStrLn "AoC"
+  s <- T.getCurrentTime
+  let !workflows = parseWorkflows $ head inp
+  let !parts = parseParts $ last inp
+  p <- T.getCurrentTime
+  let !r1 = part1 workflows parts
+  p1 <- T.getCurrentTime
+  let !r2 = part2 workflows
+
+  e <- T.getCurrentTime
+  putStrLn $ "Parsing Time: " ++ show (1e3 * realToFrac (T.nominalDiffTimeToSeconds $ T.diffUTCTime p s)) ++ " ms"
+  putStrLn $ "Part 1: " ++ show r1 ++ "; Time: " ++ show (1e3 * realToFrac (T.nominalDiffTimeToSeconds $ T.diffUTCTime p1 p)) ++ " ms"
+  putStrLn $ "Part 2: " ++ show r2 ++ "; Time: " ++ show (1e3 * realToFrac (T.nominalDiffTimeToSeconds $ T.diffUTCTime e p1)) ++ " ms"
